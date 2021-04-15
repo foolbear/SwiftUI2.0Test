059d027a1bb0284a6d38cf35ecedba8895603a0a
foolbear <greatfoolbear@gmail.com>
2021-04-15 22:42:49 +0800

    https://stackoverflow.com/a/67097502/12668737

--- SwiftUI2.0Test/ViewModelInListTestView.swift
+++ SwiftUI2.0Test/ViewModelInListTestView.swift
@@ -8,103 +8,76 @@
 import SwiftUI
 import Combine
 
-struct Item {
+struct Item: Identifiable {
     var index: Int
     var favorite: Bool
+    var id: Int { index }
 }
 
-var items = [
-    Item(index: 0, favorite: true),
-    Item(index: 1, favorite: false),
-    Item(index: 2, favorite: true),
-    Item(index: 3, favorite: false),
-    Item(index: 4, favorite: true),
-    Item(index: 5, favorite: false),
-    Item(index: 6, favorite: true),
-    Item(index: 7, favorite: false),
-//    Item(index: 8, favorite: true),
-//    Item(index: 9, favorite: false),
-//    Item(index: 10, favorite: true),
-//    Item(index: 11, favorite: false),
-//    Item(index: 12, favorite: true),
-//    Item(index: 13, favorite: false),
-//    Item(index: 14, favorite: true),
-//    Item(index: 15, favorite: false),
-//    Item(index: 16, favorite: true),
-//    Item(index: 17, favorite: false),
-//    Item(index: 18, favorite: true),
-//    Item(index: 19, favorite: false),
-]
+class Model: ObservableObject {
+    @Published var items = [
+        Item(index: 0, favorite: true),
+        Item(index: 1, favorite: false),
+        Item(index: 2, favorite: true),
+        Item(index: 3, favorite: false),
+        Item(index: 4, favorite: true),
+        Item(index: 5, favorite: false),
+        Item(index: 6, favorite: true),
+        Item(index: 7, favorite: false),
+    ]
+}
 
 struct ViewModelInListTestView: View {
+    @StateObject var model = Model()
     var body: some View {
+        print("ViewModelInListTestView refreshing"); return
         ScrollView(showsIndicators: false) {
-//            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200), spacing: 4, alignment: .center)], spacing: 4) {
-            LazyVStack {
-//            VStack {
-                ForEach(items, id: \.index) { item in
-                    ItemView(item: item)
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200), spacing: 4, alignment: .center)], spacing: 4) {
+                ForEach(model.items.indices) { index in
+                    ItemView(item: model.items[index])
+                        .environmentObject(model)
                 }
             }
         }.navigationTitle("ViewModel In List")
     }
-}
-
-struct ItemView: View {
-    let item: Item
-    @ObservedObject var viewModel: ViewModel
     
-    init(item: Item) {
-        print("ItemView.init, \(item.index)")
-        self.item = item
-        self.viewModel = ViewModel(item: item)
-    }
     
-    var body: some View {
-        HStack {
-            Text("index \(item.index)")
-            Spacer()
-            Image(systemName: viewModel.isFavoriteI ? "heart.fill" : "heart")
-                .foregroundColor(viewModel.isFavoriteI ? .red : .white)
-                .padding()
-                .onTapGesture { onFavoriteTapped() }
-                .onChange(of: viewModel.isFavoriteO) { isFavorite in
-                    print("index \(item.index), isFavorite changed as \(isFavorite)")
-                }
-        }
-        .frame(width: 200, height: 150)
-        .background(Color.gray)
-    }
-    
-    func onFavoriteTapped() {
-        viewModel.isFavoriteI.toggle()
-    }
-    
-    class ViewModel: ObservableObject {
-        @Published var isFavoriteI: Bool = false
-        @Published var isFavoriteO: Bool = false
-        private var subscriptions: Set<AnyCancellable> = []
+    struct ItemView: View {
+        @EnvironmentObject var model: Model
+        let item: Item
+        @State private var updateFavourite = PassthroughSubject<Bool, Never>()
+        @State private var favorite: Bool = false
         
         init(item: Item) {
-            print("ViewModel.init, \(item.index)")
-            let isFavorite = item.favorite
-            isFavoriteI = isFavorite; isFavoriteO = isFavorite
-            $isFavoriteI
-                .print("index \(item.index) isFavoriteI:")
-                .dropFirst()
-                .debounce(for: .milliseconds(500), scheduler: DispatchQueue.main)
-                .removeDuplicates()
-                .eraseToAnyPublisher()
-                .print("index \(item.index) isFavoriteO:")
-                .receive(on: DispatchQueue.main)
-                .assign(to: \.isFavoriteO, on: self)
-                .store(in: &subscriptions)
+            self.item = item
+            self._favorite = State(initialValue: item.favorite)
+        }
+        
+        var body: some View {
+            print("ItemView \(item.index) refreshing"); return
+            HStack {
+                Text("index \(item.index)")
+                Spacer()
+                Image(systemName: favorite ? "heart.fill" : "heart")
+                    .foregroundColor(favorite ? .red : .white)
+                    .padding()
+                    .onTapGesture {
+                        favorite.toggle()
+                        updateFavourite.send(favorite)
+                    }
+                    .onReceive(
+                        updateFavourite
+                            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+//                            .removeDuplicates()
+//                            .eraseToAnyPublisher()
+                    ) { favorite in
+                        print("set favorite as \(favorite)")
+                        model.items[item.index].favorite = favorite
+                    }
+            }
+            .frame(width: 200, height: 150)
+            .background(Color.gray)
         }
     }
-}
-
-struct ViewModelInList_Previews: PreviewProvider {
-    static var previews: some View {
-        ViewModelInListTestView()
-    }
+    
 }
