05436afd3999b9136891d73bf78250328efe9ac4
foolbear <greatfoolbear@gmail.com>
2021-08-28 17:42:09 +0800

    menu test.

--- SwiftUI2.0Test/MenuTestView.swift
+++ SwiftUI2.0Test/MenuTestView.swift
@@ -8,21 +8,44 @@
 import SwiftUI
 
 struct MenuTestView: View {
+    @State private var selectedIndex = 0
+    var colorText:[String] = ["红","黄","绿"]
+    
     var body: some View {
-        Menu {
-            Button("Hello1"){}
-            Button("Hello2"){}
-        }
-        label: {
-            Image(systemName: "folder.fill")
-            Text("PDF")
+        VStack {
+            Spacer()
+            Menu {
+                Text("Choose")
+                Button("Hello1"){}
+                Button("Hello2"){}
+            }
+            label: {
+                Image(systemName: "folder.fill")
+                Text("PDF")
+            }
+            Spacer()
+            Menu("菜单"){
+                Picker("选择颜色", selection: $selectedIndex) {
+                    ForEach(0..<self.colorText.count){
+                        index in
+                        Text(colorText[index])
+                    }
+                }
+            }
+            Spacer()
         }
         .toolbar{
             ToolbarItem(placement:ToolbarItemPlacement.automatic){
                 Menu("Menu"){
+                    Text("Choose")
                     Button("Test1"){}
                     Button("Test2"){}
                     Button("Test3"){}
+                    Menu("Menu4"){
+                        Button("Test41"){}
+                        Button("Test42"){}
+                        Button("Test43"){}
+                    }
                 }
                 .menuStyle(BorderlessButtonMenuStyle())
             }
